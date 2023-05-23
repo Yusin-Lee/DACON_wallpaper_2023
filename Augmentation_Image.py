f084b049308f95266a172570351820fcf0e45816
Yusin-Lee <imyousin12@gmail.com>
2023-05-23 10:37:26 +0900

    Augmentation image 최종 수정

--- Augmentation_Image.py
+++ Augmentation_Image.py
@@ -9,6 +9,7 @@ import PIL
 
 from torchvision import transforms as T
 import torchvision.models as models
+from sklearn.model_selection import train_test_split
 
 import warnings
 warnings.filterwarnings(action='ignore') 
@@ -24,36 +25,32 @@ def seed_everything(seed):
 
 seed_everything(CFG['SEED']) # Seed 고정
 # %%
-all_img_list = glob.glob('./도배/train/*/*')
-df = pd.DataFrame(columns=['img_path', 'label'])
-df['img_path'] = all_img_list
-df['img_path'] = df.img_path.str.replace('\\','/')
-df['label'] = df.img_path.apply(lambda x: str(x).split('/')[3])
-counts = df.groupby('label').count()
-counts
+all_img_list = glob.glob('file_path')
+train_df = pd.DataFrame(columns=['img_path', 'label'])
+train_df['img_path'] = all_img_list
+train_df['img_path'] = train_df.img_path.str.replace('\\','/')
+train_df['label'] = train_df.img_path.apply(lambda x: str(x).split('/')[3])
+counts = train_df.groupby('label').count()
+print(counts)
 # %%
-df_1 = df[df.label=='틈새과다'].reset_index(drop = True)
-df_1
+train_df_1 = train_df[train_df.label=='녹오염'].reset_index(drop = True)
+train_df_1
 transform = T.RandomApply([
-        # T.RandomHorizontalFlip(1), # 좌우반전, default_p = 0.5
-        # T.RandomVerticalFlip(1), # 상하반전, depault_p = 0.5
-        T.RandomRotation(15), # image rotate, -각도 ~ + 각도 사이를 rotate
+        # T.RandomHorizontalFlip(0.75), # 좌우반전, default_p = 0.5
+        # T.RandomVerticalFlip(0.75), # 상하반전, depault_p = 0.5
+        # T.RandomRotation(degrees=[90, 90]), # image rotate, -각도 ~ + 각도 사이를 rotate
 #        T.GaussianBlur(kernel_size=5),
-        # T.RandomAffine(15, shear=15) # image rotate + affine
+        # T.RandomAffine(45, shear=45) # image rotate + affine
 ], p = 1.0)
-image = PIL.Image.open(df_1.img_path[0])
+image = PIL.Image.open(train_df_1.img_path[0])
 image = transform(image)
 image.show()
 # %%
-for i in range(20, 50):
-    j = random.randint(0,len(df_1)-1)
-    image = PIL.Image.open(df_1.img_path[j])
+for i in range(len(train_df_1), 2*len(train_df_1)):
+    j = i - len(train_df_1)
+    # j = random.randint(0,len(train_df_1)-1)
+    image = PIL.Image.open(train_df_1.img_path[j])
     image = transform(image)
-    image.save(f'./도배/train/{df_1.label[0]}/{i}.png')
+    image.save(f'./wallpaper_relabeling/train/{train_df_1.label[0]}/{i}.png')
+    print(i)
 
-# %%
-df_1.img_path[0]
-
-# %%
-len(df_1)
-# %%
